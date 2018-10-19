e13fa33f07c05b388da4ce466b3c0b286f25392c
Kunal Madhav <kunal.madhav@xero.com>
2018-10-19 16:42:44 +1300

    Refactor simulator and command parser classes

--- lib/robot_challenge/simulator.rb
+++ lib/robot_challenge/simulator.rb
@@ -1,8 +1,4 @@
-require_relative "table"
-require_relative "robot"
-require_relative "position"
-require_relative "command_parser"
-require_relative "user_interface"
+require_relative "command"
 
 class Simulator
   attr_reader :table, :robot, :user_interface
@@ -16,26 +12,17 @@ class Simulator
   def run
     loop do
       requested_command = request_command
+      command = Command.new(args: requested_command, table: table, robot: robot)
 
-      break if terminating?(requested_command)
+      break if command.terminating?
 
-      process_request(requested_command)
+      response = command.process
+
+      report_output(response) if command.response_command?
     end
   end
 
   private
-
-  def valid_request?(command)
-    !command&.strip.length.zero?
-  end
-
-  def report_command?(command)
-    command.instance_of?(Command::Report)
-  end
-
-  def terminating?(command)
-    ["q", "quit", "exit"].include?(command.strip.downcase)
-  end
 
   def request_command
     user_interface.request_command
@@ -43,28 +30,6 @@ class Simulator
   end
 
   def report_output(output)
-    user_interface.write_message(output)
-  end
-
-  def process_request(requested_command)
-    return unless valid_request?(requested_command)
-
-    command = build_command(requested_command)
-    execute_command(command)
-  end
-
-  def build_command(requested_command)
-    parser = CommandParser.new(args: requested_command)
-    command_name = parser.parse
-
-    command_name.new(args: parser.command_args, table: table, robot: robot)
-  end
-
-  def execute_command(command)
-    output = command.execute
-
-    if output && report_command?(command)
-      report_output(output)
-    end
+    output && user_interface.write_message(output)
   end
 end
---
+++ spec/command_spec.rb
@@ -0,0 +1,148 @@
+require_relative "spec_helper"
+require_relative "../lib/robot_challenge/commands/move"
+require_relative "../lib/robot_challenge/table"
+require_relative "../lib/robot_challenge/robot"
+require_relative "../lib/robot_challenge/command"
+
+describe Command do
+  let(:table) { Table.new }
+  let(:robot) { Robot.new }
+  let(:command) { Command.new(args: args, table: table, robot: robot) }
+
+  describe "#terminating?" do
+    subject(:terminating) { command.terminating? }
+
+    context "when no command is supplied" do
+      let(:args) { "" }
+
+      it "returns false" do
+        expect(terminating).to be false
+      end
+    end
+
+    context "when the command is empty" do
+      let(:args) { " " }
+
+      it "returns false" do
+        expect(terminating).to be false
+      end
+    end
+
+    context "when given a non-termination command" do
+      let(:args) { "test" }
+
+      it "returns false" do
+        expect(terminating).to be false
+      end
+    end
+
+    context "when given a termination command" do
+      let(:args) { "test" }
+
+      it "returns true" do
+        expect(command).to receive(:termination_commands).and_return([args])
+
+        expect(terminating).to be true
+      end
+    end
+  end
+
+  describe "#process" do
+    subject(:process) { command.process }
+
+    context "when no command is supplied" do
+      let(:args) { "" }
+
+      it "returns the unknown command message" do
+        expect(process).to eq("Unknown command!")
+      end
+    end
+
+    context "when the command is empty" do
+      let(:args) { " " }
+
+      it "returns the unknown command message" do
+        expect(process).to eq("Unknown command!")
+      end
+    end
+
+    context "when the given command is invalid" do
+      let(:args) { "test^&^*" }
+
+      it "returns the unknown command message" do
+        expect(process).to eq("Unknown command!")
+      end
+    end
+
+    context "when the given the move command" do
+      let(:args) { "move" }
+
+      it "returns nil" do
+        expect(process).to be nil
+      end
+    end
+
+    context "when the given the left command" do
+      let(:args) { "left" }
+
+      it "returns nil" do
+        expect(process).to be nil
+      end
+    end
+
+    context "when the given the right command" do
+      let(:args) { "right" }
+
+      it "returns nil" do
+        expect(process).to be nil
+      end
+    end
+
+    context "when the given the place command" do
+      let(:args) { "place 1,1,north" }
+
+      it "returns the position" do
+        expect(process).to_not be nil
+      end
+    end
+
+    context "when the given the report command" do
+      let(:args) { "report" }
+      let(:position) { Position.new(x: 1, y: 1, orientation: Position::NORTH) }
+
+      before { robot.move_to(position) }
+
+      it "returns the robots position" do
+        expect(process).to eq("Output: 1,1,NORTH")
+      end
+    end
+  end
+
+  describe "#response_command?" do
+    subject(:response_command) { command.response_command? }
+
+    context "when the report command is supplied" do
+      let(:args) { "report" }
+
+      it "returns true" do
+        expect(response_command).to be true
+      end
+    end
+
+    context "when an unknown command is supplied" do
+      let(:args) { "test" }
+
+      it "returns true" do
+        expect(response_command).to be true
+      end
+    end
+
+    context "when the move command is supplied" do
+      let(:args) { "move" }
+
+      it "returns false" do
+        expect(response_command).to be false
+      end
+    end
+  end
+end
